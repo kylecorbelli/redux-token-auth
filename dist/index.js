c4a7ba7624ad5513ae37ad1ba1db4fc8a10aa029
Kyle Corbelli <kyle.corbelli@gmail.com>
2017-09-18 15:34:01 -0700

    Recompile and bump version

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,6 @@ var actions_1 = require("./actions");
 exports.generateAuthActions = actions_1.default;
 var reducers_1 = require("./reducers");
 exports.reduxTokenAuthReducer = reducers_1.default;
+var generate_require_signin_wrapper_1 = require("./generate-require-signin-wrapper");
+exports.generateRequireSignInWrapper = generate_require_signin_wrapper_1.default;
 //# sourceMappingURL=index.js.map
