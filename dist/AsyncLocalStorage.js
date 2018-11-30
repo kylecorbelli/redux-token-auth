124d0fb83b05efdaf92658244cc063aed854a859
zack <zack@shergonprint.com>
2018-11-30 16:10:37 +0800

    Auto switch to AsyncStorage for react-native.

--- dist/AsyncLocalStorage.js
+++ dist/AsyncLocalStorage.js
@@ -1,69 +1,119 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var react_native_1 = require("react-native");
 var AsyncLocalStorage = {
     getItem: function (key) { return new Promise(function (resolve, reject) {
-        try {
-            var value = window.localStorage.getItem(key);
-            resolve(value);
+        if (window.localStorage) {
+            try {
+                var value = window.localStorage.getItem(key);
+                resolve(value);
+            }
+            catch (error) {
+                reject(error);
+            }
         }
-        catch (error) {
-            reject(error);
+        else {
+            react_native_1.AsyncStorage.getItem(key).then(function (value) {
+                resolve(value);
+            }, reject);
         }
     }); },
     setItem: function (key, value) { return new Promise(function (resolve, reject) {
-        try {
-            window.localStorage.setItem(key, value);
-            resolve(value);
+        if (window.localStorage) {
+            try {
+                window.localStorage.setItem(key, value);
+                resolve(value);
+            }
+            catch (error) {
+                reject(error);
+            }
         }
-        catch (error) {
-            reject(error);
+        else {
+            react_native_1.AsyncStorage.setItem(key, value).then(function () {
+                resolve(value);
+            }, reject);
         }
     }); },
     removeItem: function (key) { return new Promise(function (resolve, reject) {
-        try {
-            var value = window.localStorage.getItem(key);
-            window.localStorage.removeItem(key);
-            resolve(value);
+        if (window.localStorage) {
+            try {
+                var value = window.localStorage.getItem(key);
+                window.localStorage.removeItem(key);
+                resolve(value);
+            }
+            catch (error) {
+                reject(error);
+            }
         }
-        catch (error) {
-            reject(error);
+        else {
+            react_native_1.AsyncStorage.removeItem(key).then(function () {
+                resolve();
+            }, reject);
         }
     }); },
     clear: function () { return new Promise(function (resolve, reject) {
-        try {
-            window.localStorage.clear();
-            resolve(true);
+        if (window.localStorage) {
+            try {
+                window.localStorage.clear();
+                resolve(true);
+            }
+            catch (error) {
+                reject(error);
+            }
         }
-        catch (error) {
-            reject(error);
+        else {
+            react_native_1.AsyncStorage.clear().then(function () {
+                resolve(true);
+            }, reject);
         }
     }); },
     getAllKeys: function () { return new Promise(function (resolve, reject) {
-        try {
-            var allKeys = Object.keys(window.localStorage);
-            resolve(allKeys);
+        if (window.localStorage) {
+            try {
+                var allKeys = Object.keys(window.localStorage);
+                resolve(allKeys);
+            }
+            catch (error) {
+                reject(error);
+            }
         }
-        catch (error) {
-            reject(error);
+        else {
+            react_native_1.AsyncStorage.getAllKeys().then(function (allKeys) {
+                resolve(allKeys);
+            }, reject);
         }
     }); },
     multiGet: function (keys) { return new Promise(function (resolve, reject) {
-        try {
-            var values = keys.map(function (key) { return [key, window.localStorage.getItem(key)]; });
-            resolve(values);
+        if (window.localStorage) {
+            try {
+                var values = keys.map(function (key) { return [key, window.localStorage.getItem(key)]; });
+                resolve(values);
+            }
+            catch (error) {
+                reject(error);
+            }
         }
-        catch (error) {
-            reject(error);
+        else {
+            react_native_1.AsyncStorage.multiGet(keys).then(function (values) {
+                resolve(values);
+            }, reject);
         }
     }); },
     multiSet: function (keyValuePairs) { return new Promise(function (resolve, reject) {
-        try {
-            keyValuePairs.forEach(function (keyValuePair) { return window.localStorage.setItem(keyValuePair[0], keyValuePair[1]); });
-            var newKeyValuePairs = keyValuePairs.map(function (keyValuePair) { return [keyValuePair[0], window.localStorage.getItem(keyValuePair[0])]; });
-            resolve(newKeyValuePairs);
+        if (window.localStorage) {
+            try {
+                keyValuePairs.forEach(function (keyValuePair) { return window.localStorage.setItem(keyValuePair[0], keyValuePair[1]); });
+                var newKeyValuePairs = keyValuePairs.map(function (keyValuePair) { return [keyValuePair[0], window.localStorage.getItem(keyValuePair[0])]; });
+                resolve(newKeyValuePairs);
+            }
+            catch (error) {
+                reject(error);
+            }
         }
-        catch (error) {
-            reject(error);
+        else {
+            react_native_1.AsyncStorage.multiSet(keyValuePairs).then(function () {
+                resolve();
+            }, reject);
         }
     }); },
 };
